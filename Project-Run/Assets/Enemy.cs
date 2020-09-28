a1aecfcec24efe67243ea5b26c2ca7e72916ea35
Eric Wade <ewade@stu.jsu.edu>
2020-09-28 10:06:19 -0500

    Enemy goes to Jump Location

--- Project-Run/Assets/Enemy.cs
+++ Project-Run/Assets/Enemy.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
+    public GameObject target;
+    // get the target within code but for now, do it by a*
+    public NavMeshAgent agent;
+
+    private void jump(){
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
     }
 
     // Update is called once per frame
