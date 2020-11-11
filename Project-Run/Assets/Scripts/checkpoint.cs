d492a3b5c1cc3815d00b0a3f58cd516dc5841d29
Eric Wade <ewade@stu.jsu.edu>
2020-11-11 10:38:09 -0600

    this is a test
    test

---
+++ Project-Run/Assets/Scripts/checkpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoint : MonoBehaviour
+{
+    public Vector3 respawnPoint;
+    
+    public void die(){
+        this.transform.position = respawnPoint;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("checkpoint")){
+            respawnPoint = other.gameObject.transform.position;
+        }
+    }
+}
